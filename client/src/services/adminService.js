abf29eafb8990050270c46d399bac1ba11511b54
Tohid Ladman <tohidladman76@gmail.com>
2026-03-15 09:26:38 +0530

    admin dashboard integration

--- client/src/services/adminService.js
+++ client/src/services/adminService.js
@@ -14,3 +14,18 @@ export const getAdminDashboard = async () => {
   const response = await api.get("/admin/dashboard");
   return response.data;
 };
+
+export const getAdminUsers = async () => {
+  const response = await api.get("/admin/users");
+  return response.data;
+};
+
+export const getAdminHelpers = async () => {
+  const response = await api.get("/admin/helpers");
+  return response.data;
+};
+
+export const getAdminIssues = async () => {
+  const response = await api.get("/admin/issues");
+  return response.data;
+};
